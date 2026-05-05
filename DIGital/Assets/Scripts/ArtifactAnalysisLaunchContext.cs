6bbed909d299dfca3b8aca584613581a6114c2f2
trw286 <trw286@nau.edu>
2026-05-04 20:20:31 -0700

    Created ArtifactAnalysisLaunchContext script
    Script that detects where the analysis scene was open/called from.
    
    This way, certain workflows in the analysis scene can be controlled. For example, a Continue button will be visible if the analysis scene is called from the collection form so the user can continue excavating.

---
+++ DIGital/Assets/Scripts/ArtifactAnalysisLaunchContext.cs
@@ -0,0 +1,80 @@
+using System;
+
+// different states of the analysis scene
+    // final analysis is after exacvation and backfill
+    // user submission is when the analysis scene opens from a user recording
+    // answer key when shown from the collection form
+public enum ArtifactAnalysisStart
+{
+    FinalAnalysis,
+    UserSubmissionFromExcavation,
+    AnswerKeyFromExcavation
+}
+
+public static class ArtifactAnalysisLaunchContext
+{
+    public static ArtifactAnalysisStart StartMode = ArtifactAnalysisStart.FinalAnalysis;
+    
+    public static ArtifactRecord UserSubmisson;
+    public static string ArtifactId;
+
+    // see where analysis scene was launched
+    // for Continue button visability
+    public static bool LaunchedFromExcavation
+    {
+        get
+        {
+            return StartMode == ArtifactAnalysisStart.UserSubmissionFromExcavation ||
+                   StartMode == ArtifactAnalysisStart.AnswerKeyFromExcavation;
+        }
+    }
+
+    public static void Clear()
+    {
+        StartMode = ArtifactAnalysisStart.FinalAnalysis;
+        UserSubmisson = null;
+        ArtifactId = null;
+    }
+}
+
+// data
+[Serializable]
+public class ArtifactRecord
+{
+    public int id;
+    public string date_discovered;
+    public string investigator;
+    public string area;
+    public string unit;
+    public string layer;
+    public string site;
+    public string associated_features;
+    public string decorative_tech;
+    public string material;
+    public string firing;
+    public string paint;
+    public string cultural_affiliation;
+    public string object_class;
+    public string bag_number;
+    public string artifact_id;
+    public string created_at;
+    public string updated_at;
+    public string user_id;
+}
+
+[Serializable]
+public class SingleArtifactResponse
+{
+    public bool ok;
+    public string message;
+    public ArtifactRecord artifact;
+    public string error;
+}
+
+[Serializable]
+public class ArtifactListResponse
+{
+    public bool ok;
+    public ArtifactRecord[] artifacts;
+    public string error;
+}
